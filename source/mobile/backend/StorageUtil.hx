60d59c4eaa1fefcacaf4758f6f3c65d9f24c1964
HomuHomu833 <egzozu.be1.bas@gmail.com>
2025-04-14 14:32:47 +0300

    add mods in .PsychEngine073 creation

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -85,8 +85,8 @@ class StorageUtil
 
 		try
 		{
-			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory()))
-				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory());
+			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory() + 'mods'))
+				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory() + 'mods');
 		}
 		catch (e:Dynamic)
 		{
