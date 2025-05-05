c5a4ec6e5a8c3f4584f0c38ec32d5e36ea882074
Ejrjrlq <96973667+Newbion2@users.noreply.github.com>
2025-05-06 00:41:03 +0900

    Update StorageUtil.hx

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -55,7 +55,7 @@ class StorageUtil
 	#if android
 	// always force path due to haxe
 	public static function getExternalStorageDirectory():String
-		return '/sdcard/.PsychEngine073/';
+		return '/sdcard/.PsychEnginePort/';
 
 	public static function requestPermissions():Void
 	{
